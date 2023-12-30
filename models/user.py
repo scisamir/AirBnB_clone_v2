075559e345e00fb9157317a1b3220bf4adbf2f48
Samir Idris <samzidris11@gmail.com>
2023-12-30 00:37:11 -0800

    make first_name and last_name optional

--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship('Place', backref='user', cascade='all, delete')
     reviews = relationship('Review', backref='user', cascade='all, delete')
