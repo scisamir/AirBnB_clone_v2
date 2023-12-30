bccea6886f2e5e4235497a903c3446519e1d642e
Samir Idris <samzidris11@gmail.com>
2023-12-30 12:46:26 -0800

    fix and update base_model handling of kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -26,12 +26,16 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key in kwargs.keys():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        self.__dict__[key] = datetime.strptime(
+                            kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        self.__dict__[key] = kwargs[key]
+            self.created_at = kwargs.get('created_at', datetime.now())
+            self.updated_at = kwargs.get('updated_at', datetime.now())
+            self.id = kwargs.get('id', str(uuid.uuid4()))
 
     def __str__(self):
         """Returns a string representation of the instance"""
